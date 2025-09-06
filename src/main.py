9db8b97a6ac9573ef4b4e3d09016535ae7bcf772
fortibit <86564794+fortibit@users.noreply.github.com>
2025-09-06 13:16:28 +0200

    Add conversion function and first test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from textnode import TextNode, TextType
-
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
     print(TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev"))
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextType(unittest.TestCase):
@@ -69,6 +69,14 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(
             "TextNode(hi, bold, None)", repr(node)
         )
+
+
+# --- text_node_to_html_node ---
+def test_text(self):
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
 
 
 if __name__ == "__main__":
